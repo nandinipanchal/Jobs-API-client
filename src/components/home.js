f6671db7f019ebfab89ef7873aa6026d5747dc96
Nandini panchal <nandini.sanvirp@gmail.com>
2021-11-02 15:30:55 +0530

    public jobs rendered on home page

--- src/components/home.js
+++ src/components/home.js
@@ -3,50 +3,69 @@ import '../index.css'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Job from './jobs/job'
+import PublicJob from './publicjob'
 
 const Home = () => {
-    const [state, setstate] = useState({
-        company : '',
-        position :''
+    const [jobDetail, setJobDetail] = useState({
+        company: '',
+        position: ''
     })
-   
+
+    const [state, setState] = useState([])
+
+    const addJobs = (NewJob) => {
+        setState((prevJobs) => {
+            return [...prevJobs, NewJob]
+        })
+    }
 
     useEffect(() => {
         axios.get('http://localhost:7000/api/v1')
             .then((response) => {
-                console.log(response)
-                console.log(response.data.jobs)
+                // console.log(response)
+                // console.log(response.data.jobs)
+                console.log('testing 0')
                 let job = response.data.jobs
-          
-                job.map((item,index)=>{
+
+                console.log(job)
+
+                job.map((item) => {
                     console.log(item.company)
                     console.log(item.position)
+                    jobDetail.company = item.company
+                    jobDetail.position = item.position
+                    setJobDetail(jobDetail)
 
-                }) 
-        
-            }) 
+                    addJobs(jobDetail)
+
+                })
+
+            })
             .catch((error) => console.error(error))
     }, [])
 
+    console.log('testing 1')
+    console.log(state)
+
     return (<div>
-        <header>
+        <header className ='publicheader'>
             <Link to='./login'><button>Log In</button></Link>
         </header>
-        <h1>This is a home page</h1>
-        <p>Company :{state.company}</p>
-        <p>Position :{state.position}</p>
+       
+        {
+            state.map(item => {
+                return (
+                    <PublicJob
+                        company={item.company}
+                        position={item.position}
+                    />)
+            })
+        }
+
     </div>)
 }
 
 export default Home
 
-//http://localhost:7000/api/v1
 
-// fetch('http://localhost:7000/api/v1', {
-//             method: 'GET'
-//           })
-//           .then((res) => res.json())
-//           .then( (data) => console.log(data ))
-//           .catch((error) => {
-//             console.error('Error:', error);
-//           })
+
--- src/components/jobs/dashboard.js
+++ src/components/jobs/dashboard.js
@@ -2,23 +2,23 @@ import react, { useState } from 'react'
 import Dashboardform from './dashboardform'
 import Job from './job'
 
-const Dashboard = () =>{
-    const [jobs ,setJobs] = useState([])
-    const addJobs =(NewJob)=>{
-        setJobs((prevJobs)=>{
-            return [...prevJobs ,NewJob]
+const Dashboard = () => {
+    const [jobs, setJobs] = useState([])
+    const addJobs = (NewJob) => {
+        setJobs((prevJobs) => {
+            return [...prevJobs, NewJob]
         })
     }
 
     return (<div>
-        <Dashboardform onAdd={addJobs}/>
-        {jobs.map((jobitem,index) =>{
+        <Dashboardform onAdd={addJobs} />
+        {jobs.map((jobitem, index) => {
             return (
                 <Job
-                key ={1}
-                id ={index}
-                company ={jobitem.cname}
-                position ={jobitem.pname}
+                    key={1}
+                    id={index}
+                    company={jobitem.cname}
+                    position={jobitem.pname}
 
                 />
             )
